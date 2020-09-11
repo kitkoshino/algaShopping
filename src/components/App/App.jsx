978982463ef6d8b0e48f6c729613e249c436f3e5
cristianekoshino <cristianekoshino@gmail.com>
2020-09-11 19:27:24 +0100

    add checkbox shared component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Wrapper, Container } from './App.styles';
 import AppHeader from '../AppHeader/AppHeader';
 import AppContainer from '../AppContainer/AppContainer';
+import Checkbox from '../../shared/Checkbox/Checkbox';
 
 function App() {
+  const [lettuce, setLettuce] = useState(true);
+  const [rice, setRice] = useState(false);
+
+
+
   return (
     <Wrapper>
       <Container>
         <AppHeader />
-        <AppContainer 
-          left={<div style={{backgroundColor: 'red'}}>produtos disponiveis</div>}
-          middle={<div style={{backgroundColor: 'green'}}>sua lista de compras</div>}
-          right={<div style={{backgroundColor: 'blue'}}>estatísticas</div>}
+        <AppContainer
+          left={
+            <div>
+              produtos disponiveis 
+              <Checkbox value={lettuce} title="Alface" onClick={() => setLettuce(!lettuce)}/>
+              <Checkbox value={rice} title="Arroz"onClick={() => setRice(!rice)} />
+            </div>
+          }
+          middle={<div>sua lista de compras</div>}
+          right={<div>estatísticas</div>}
         />
       </Container>
     </Wrapper>
